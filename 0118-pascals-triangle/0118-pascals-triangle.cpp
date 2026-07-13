be48b3a26c6ed45518f6ee60df9d858b8316a9bf
Mohammad Saad <saadhussain.agr@gmail.com>
2026-07-13 23:36:21 +0530

    Time: 3 ms (7.9%), Space: 9.7 MB (63.65%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-   int ncr(int n, int r)
-   {
-    int res=1;
-    for(int i=0;i<r;i++)
-    {
-        res*=(n-i);
-    res=res/(i+1);
- }
- return res;
-   }
-
-   vector<vector<int>> generate(int numRows) {
-     vector<vector<int>> ans;
-     int element;
-     int i,j;
-      for(i=0;i<numRows;i++)
-      {
-        vector<int>row;
-         for(j=0;j<i+1;j++)
-        {element=ncr(i,j);
-        row.push_back(element);
-      }
-      ans.push_back(row) ;
-
-      }
-      return ans;
-   }
-    };
+    vector<vector<int>> generate(int numRows) { //basically, take one element as 1; then keep updating it,
+    //push into row vector, then final row vector into asnwer vector
+    vector<vector<int>> ans;
+    for(int i=0;i<numRows;i++)
+    {   vector<int> row;
+        int element=1;
+        for(int j=0;j<i+1;j++)
+        {
+            row.push_back(element);
+            element=element*(i-j)/(j+1); //FORMULA
+        }
+        ans.push_back(row);
+    }
+    return ans;
+        
+    }
+};
