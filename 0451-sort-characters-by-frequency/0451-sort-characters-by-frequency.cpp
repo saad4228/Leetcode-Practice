2fbb6199a44187e8ddafff597a238f3e90836fa3
Mohammad Saad <saadhussain.agr@gmail.com>
2026-08-12 01:37:09 +0530

    Time: 4 ms (37.45%), Space: 17.3 MB (5.38%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freq;
+
+        for (char c : s) {
+            freq[c]++;
+        }
+        vector<vector<char>> bucket(s.size()+1);
+        for( auto it: freq)
+        {
+            bucket[it.second].push_back(it.first);
+        }
+
+        string ans = "";
+
+        for (int i=s.size();i>=1;i--)
+       { for(char c: bucket[i])
+        {
+            ans += string(i,c);
+        }}
+
+        return ans;
+    }
+};
